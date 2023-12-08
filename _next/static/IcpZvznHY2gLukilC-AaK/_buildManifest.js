27f945b718a55c64a4e32a034807c6d3d7faa68a
C-Johanson <119763341+C-Johanson@users.noreply.github.com>
2023-12-08 18:27:16 +0000

    Deploying to gh-pages from @ C-Johanson/nextjs_gh-pages_site@8f3f3a0efc6fb42bc8363ada43e10f2efa88dca8 🚀

---
+++ _next/static/IcpZvznHY2gLukilC-AaK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-b36363caa78a9630.js","static/chunks/d7eeaac4-8abb1ed03b931ecc.js","static/chunks/pages/index-361beea36280aafd.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
